62d60c78a4e3b7d85a25986953c8a632f3f90408
Jamie-Groves <jcg5v@mtmail.mtsu.edu>
2025-04-30 20:50:37 -0500

    Added files for final project submission

---
+++ Library Database.sql
@@ -0,0 +1,110 @@
+CREATE DATABASE  IF NOT EXISTS `library_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `library_database`;
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library_database
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `copies_available` int NOT NULL,
+  `checked_out_by` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'A Game Of Thrones',4,NULL),(2,'A Clash of Kings',1,NULL),(3,'A Storm of Swords',5,NULL),(4,'A Feast for Crows',5,NULL),(5,'A Dance with Dragons',2,NULL),(6,'Dune',1,NULL),(7,'Dune Messiah',4,NULL),(8,'Children of Dune',4,NULL),(9,'The Lord of the Rings',5,NULL),(10,'The Two Towers',2,NULL),(11,'The Return of the King',1,NULL),(12,'The Hobbit',2,NULL),(13,'The Design of Everyday Things',1,NULL),(14,'Don\'t Make Me Think',4,NULL),(15,'The Name of the Wind',2,NULL),(16,'The Wise Man\'s Fear',1,NULL),(17,'The Way of Kings',5,NULL),(18,'Words of Radiance',5,NULL),(19,'Oathbringer',2,NULL),(20,'Rhythm of War',4,NULL),(21,'Elantris',3,NULL),(22,'Warbreaker',1,NULL),(23,'Mistborn: The Final Empire',2,NULL),(24,'The Well of Ascension',5,NULL),(25,'The Hero of Ages',1,NULL),(26,'The Lies of Locke Lamora',4,NULL),(27,'Red Seas Under Red Skies',4,NULL),(28,'Republic of Thieves',4,NULL),(29,'The Blade Itself',3,NULL),(30,'Before They Are Hanged',4,NULL),(31,'Last Argument of Kings',4,NULL),(32,'The Black Prism',1,NULL),(33,'The Blinding Knife',2,NULL),(34,'The Broken Eye',3,NULL);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checked_out`
+--
+
+DROP TABLE IF EXISTS `checked_out`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checked_out` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `checkout_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `checked_out_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `checked_out_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checked_out`
+--
+
+LOCK TABLES `checked_out` WRITE;
+/*!40000 ALTER TABLE `checked_out` DISABLE KEYS */;
+INSERT INTO `checked_out` VALUES (7,1,10,'2025-01-28 15:25:58'),(9,2,16,'2025-04-15 16:51:17'),(10,1,5,'2025-04-20 21:49:29');
+/*!40000 ALTER TABLE `checked_out` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'jamietest','jamietest@test.com'),(2,'usertest','usertest@testing.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-30 20:42:01
---
+++ README.md
@@ -0,0 +1,13 @@
+REQUIRED: MySQLWorkbench , Visual Studio Code
+
+VISUAL STUDIO CODE NuGET PACKAGES:
+    Emgu.CV
+    Emgu.CV.runtime.windows
+    MySQL.Data
+    MySQLConnector
+
+1) Open MySQLWorkbench and create a database using the provide .sql file (please remember the password and username created as you will need to insert these into line 34 of the code)
+2) Open the project solution with visual studio
+3) Make sure all the required NuGet Packages are installed
+4) Edit line 34 of Form1.cs to be the information of your created database
+5) Run the application
